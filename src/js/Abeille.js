5fcd68acb49ba9a6ca616b0fe7d98b1e81355adc
baptiste <baptiste.brillet@epfedu.fr>
2025-03-12 22:17:23 +0100

    V0 Mairie + Abeille (bug)

--- src/js/Abeille.js
+++ src/js/Abeille.js
@@ -16,8 +16,8 @@ export default class Abeille extends Phaser.Scene {
     const CarteAbeille = this.add.tilemap("carteAbeille");
     // chargement du jeu de tuiles
     const tileset = CarteAbeille.addTilesetImage(
-      "TileSet_VF",
-      "Phaser_TileSet"
+      "TileSet_VF2",
+      "Phaser_TileSetV2"
     );
     // chargement de chaque calque
     const Fond = CarteAbeille.createLayer(
@@ -48,33 +48,76 @@ export default class Abeille extends Phaser.Scene {
       "Arbre",
       tileset
     );
-  
+
     // définition des tuiles de plateformes qui sont solides
     const objects = [Fond, FondTerre, FondArbre, Foret, Trou, Plateforme, Arbre];
     objects.forEach(obj => obj.setCollisionByProperty({ estSolide: true }));
-  
+
+    /****************************
+    *  RECUPERATION DU PERSONNAGE  *
+    ****************************/
+
+    // On créée un nouveeau personnage : player
+    this.player = this.physics.add.sprite(300, 400, "img_perso");
+    this.player.setBounce(0.2); // on donne un petit coefficient de rebond
+    this.player.setCollideWorldBounds(true); // le player se cognera contre les bords du monde
+
+
+    /***********************
+     *  CREATION DU CLAVIER *
+     ************************/
+    // ceci permet de creer un clavier et de mapper des touches, connaitre l'état des touches
+    this.clavier = this.input.keyboard.createCursorKeys();
+
+    /*****************************************************
+     *  GESTION DES INTERATIONS ENTRE  GROUPES ET ELEMENTS *
+     ******************************************************/
+
+    //  Collide the player and the groupe_etoiles with the groupe_plateformes
+    this.physics.add.collider(this.player, objects);
+
   }
 
   update() {
-    if (this.clavier.left.isDown) {
+    // Gestion du clavier
+    this.clavier = this.input.keyboard.createCursorKeys();
+    this.keys = this.input.keyboard.addKeys({
+      up: Phaser.Input.Keyboard.KeyCodes.Z, //Touche Z pour avancer
+      down: Phaser.Input.Keyboard.KeyCodes.S, //Touche S pour reculer
+      left: Phaser.Input.Keyboard.KeyCodes.Q, //Touche Q pour aller à gauche
+      right: Phaser.Input.Keyboard.KeyCodes.D //Touche D pour aller à droite
+    });
+
+    if (this.keys.left.isDown) {
       this.player.setVelocityX(-160);
-      this.player.anims.play("anim_tourne_gauche", true);
-    } else if (this.clavier.right.isDown) {
+      this.player.anims.play("anim_gauche", true);
+    } else if (this.keys.right.isDown) {
       this.player.setVelocityX(160);
-      this.player.anims.play("anim_tourne_droite", true);
+      this.player.anims.play("anim_droite", true);
     } else {
       this.player.setVelocityX(0);
-      this.player.anims.play("anim_face");
-    }
-    if (this.clavier.up.isDown && this.player.body.touching.down) {
-      this.player.setVelocityY(-330);
     }
 
-    if (Phaser.Input.Keyboard.JustDown(this.clavier.space) == true) {
-      if (this.physics.overlap(this.player, this.porte_retour)) {
-        console.log("niveau 3 : retour vers selection");
-        this.scene.switch("selection");
-      }
+    // Déplacement vertical
+    if (this.keys.up.isDown) {
+      this.player.setVelocityY(-160);
+      this.player.anims.play("anim_dos", true);
+    } else if (this.keys.down.isDown) {
+      this.player.setVelocityY(160);
+      this.player.anims.play("anim_face", true);
+    } else {
+      this.player.setVelocityY(0);
+    }
+    // redimentionnement du monde avec les dimensions calculées via tiled
+    this.physics.world.setBounds(0, 0, 640, 3200);
+    //  ajout du champs de la caméra de taille identique à celle du monde
+    this.cameras.main.setBounds(0, 0, 640, 3200);
+    // ancrage de la caméra sur le joueur
+    this.cameras.main.startFollow(this.player);
+
+    // Si aucune touche n'est pressée, jouer l'animation de repos
+    if (this.player.body.velocity.x === 0 && this.player.body.velocity.y === 0) {
+      this.player.anims.play("anim_repos");
     }
   }
 }
--- src/js/Mairie.js
+++ src/js/Mairie.js
@@ -9,11 +9,11 @@ export default class Mairie extends Phaser.Scene {
     this.load.image("Phaser_TileSet", "src/assets/TileSet_VF.png");
     this.load.tilemapTiledJSON("carteMairie", "src/assets/mairie.json");
   }
- 
+
   /***********************************************************************/
   /** FONCTION CREATE
 /***********************************************************************/
- 
+
   /* La fonction create est appelée lors du lancement de la scene
    * si on relance la scene, elle sera appelée a nouveau
    * on y trouve toutes les instructions permettant de créer la scene
@@ -27,7 +27,7 @@ export default class Mairie extends Phaser.Scene {
      *  CREATION DU MONDE  *
      *************************************/
     const CarteMairie = this.add.tilemap("carteMairie");
- 
+
     // chargement du jeu de tuiles
     const tileset = CarteMairie.addTilesetImage(
       "TileSet_VF",
@@ -78,9 +78,9 @@ export default class Mairie extends Phaser.Scene {
       "objet",
       tileset
     );
- 
+
     // définition des tuiles de plateformes qui sont solides
- 
+
     const objects = [plancher, mur, mur2, escalier, Calque_de_Tuiles_7, escalier2, fenetre2, tapis, meuble, tapis2, objet];
     objects.forEach(obj => obj.setCollisionByProperty({ estSolide: true }));
 
@@ -89,105 +89,71 @@ export default class Mairie extends Phaser.Scene {
     /****************************
      *  RECUPERATION DU PERSONNAGE  *
      ****************************/
- 
+
     // On créée un nouveeau personnage : player
     this.player = this.physics.add.sprite(300, 400, "img_perso");
- 
-    //  propriétées physiqyes de l'objet player :
     this.player.setBounce(0.2); // on donne un petit coefficient de rebond
     this.player.setCollideWorldBounds(true); // le player se cognera contre les bords du monde
- 
-    /***************************
-     *  CREATION DES ANIMATIONS *
-     ****************************/
-    // dans cette partie, on crée les animations, à partir des spritesheet
-    // chaque animation est une succession de frame à vitesse de défilement défini
-    // une animation doit avoir un nom. Quand on voudra la jouer sur un sprite, on utilisera la méthode play()
-    // creation de l'animation "anim_tourne_gauche" qui sera jouée sur le player lorsque ce dernier tourne à gauche
-    this.anims.create({
-      key: "anim_tourne_gauche", // key est le nom de l'animation : doit etre unique poru la scene.
-      frames: this.anims.generateFrameNumbers("img_perso", {
-        start: 0,
-        end: 3
-      }), // on prend toutes les frames de img perso numerotées de 0 à 3
-      frameRate: 10, // vitesse de défilement des frames
-      repeat: -1 // nombre de répétitions de l'animation. -1 = infini
-    });
- 
-    // creation de l'animation "anim_tourne_face" qui sera jouée sur le player lorsque ce dernier n'avance pas.
-    this.anims.create({
-      key: "anim_face",
-      frames: [{ key: "img_perso", frame: 4 }],
-      frameRate: 20
-    });
- 
-    // creation de l'animation "anim_tourne_droite" qui sera jouée sur le player lorsque ce dernier tourne à droite
-    this.anims.create({
-      key: "anim_tourne_droite",
-      frames: this.anims.generateFrameNumbers("img_perso", {
-        start: 5,
-        end: 8
-      }),
-      frameRate: 10,
-      repeat: -1
-    });
- 
+
+    
     /***********************
      *  CREATION DU CLAVIER *
      ************************/
     // ceci permet de creer un clavier et de mapper des touches, connaitre l'état des touches
     this.clavier = this.input.keyboard.createCursorKeys();
- 
+
     /*****************************************************
      *  GESTION DES INTERATIONS ENTRE  GROUPES ET ELEMENTS *
      ******************************************************/
- 
+
     //  Collide the player and the groupe_etoiles with the groupe_plateformes
-    this.physics.add.collider(player, objects);
+    this.physics.add.collider(this.player, objects);
   }
- 
+
   /***********************************************************************/
   /** FONCTION UPDATE
 /***********************************************************************/
- 
+
   update() {
- 
-    if (clavier.left.isDown) {
-      player.setVelocityX(-160);
-      player.anims.play("anim_tourne_gauche", true);
- 
-    } else if (clavier.right.isDown) {
-      player.setVelocityX(160);
-      player.anims.play("anim_tourne_droite", true);
+    // Gestion du clavier
+    this.clavier = this.input.keyboard.createCursorKeys();
+    this.keys = this.input.keyboard.addKeys({
+      up: Phaser.Input.Keyboard.KeyCodes.Z, //Touche Z pour avancer
+      down: Phaser.Input.Keyboard.KeyCodes.S, //Touche S pour reculer
+      left: Phaser.Input.Keyboard.KeyCodes.Q, //Touche Q pour aller à gauche
+      right: Phaser.Input.Keyboard.KeyCodes.D //Touche D pour aller à droite
+    });
+
+    if (this.keys.left.isDown) {
+      this.player.setVelocityX(-160);
+      this.player.anims.play("anim_gauche", true);
+    } else if (this.keys.right.isDown) {
+      this.player.setVelocityX(160);
+      this.player.anims.play("anim_droite", true);
     } else {
-      player.setVelocityX(0);
-      if (player.body.velocity.y == 0)
-        player.anims.play("anim_face");
-    }
- 
-    if (clavier.up.isDown) {
-      player.setVelocityY(-160);
-      player.anims.play("anim_face", true);
-    }
-    else if (clavier.down.isDown) {
-      player.setVelocityY(160);
-      player.anims.play("anim_face", true);
-    }
-    else {
-      player.setVelocityY(0);
-      if (player.body.velocity.x == 0)
-        player.anims.play("anim_face");
+      this.player.setVelocityX(0);
     }
 
+    // Déplacement vertical
+    if (this.keys.up.isDown) {
+      this.player.setVelocityY(-160);
+      this.player.anims.play("anim_dos", true);
+    } else if (this.keys.down.isDown) {
+      this.player.setVelocityY(160);
+      this.player.anims.play("anim_face", true);
+    } else {
+      this.player.setVelocityY(0);
+    }
+    // redimentionnement du monde avec les dimensions calculées via tiled
+    this.physics.world.setBounds(0, 0, 640, 640);
+    //  ajout du champs de la caméra de taille identique à celle du monde
+    this.cameras.main.setBounds(0, 0, 640, 640);
+    // ancrage de la caméra sur le joueur
+    this.cameras.main.startFollow(this.player);
 
-
-    if (Phaser.Input.Keyboard.JustDown(clavier.space) == true) {
-      if (this.physics.overlap(player, this.porte1))
-        this.scene.switch("niveau1");
-      if (this.physics.overlap(player, this.porte2))
-        this.scene.switch("niveau2");
-      if (this.physics.overlap(player, this.porte3))
-        this.scene.switch("niveau3");
+    // Si aucune touche n'est pressée, jouer l'animation de repos
+    if (this.player.body.velocity.x === 0 && this.player.body.velocity.y === 0) {
+      this.player.anims.play("anim_repos");
     }
   }
 }
